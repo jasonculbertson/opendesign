4134361b50b56873057d2c9ae12130575f05dfb4
Jason Culbertson <jason.culbertson@linktr.ee>
2024-12-22 18:55:28 -0800

    Update pageUtils.ts

--- src/utils/pageUtils.ts
+++ src/utils/pageUtils.ts
@@ -14,7 +14,15 @@ export function shouldShowSubstack(pathname: string): boolean {
   // Don't show on pages that are known to have L3 content
   const pagesWithL3Content = [
     '/docs/team/recruiting/job-descriptions-jds',
-    // Add other L2 pages that have L3 content here
+    '/docs/team/design',           // Has design-sprint, t-shirt-sizing
+    '/docs/team/product',          // Has one-pagers, product-spec, etc.
+    '/docs/team/recruiting',       // Has job-descriptions-jds
+    '/docs/leadership/month-1',    // Has level-competencies
+    '/docs/leadership/quarter-1',  // Has sub-pages
+    '/docs/leadership/quarter-2',  // Has sub-pages
+    '/docs/leadership/week-1-2',   // Has sub-pages
+    '/docs/leadership/day-1',      // Has sub-pages
+    '/docs/leadership/departure'   // Has sub-pages
   ];
   
   return !pagesWithL3Content.includes(pathname);
