43252aba579da4a6e3b8c3c47d0b87ad5a98eff1
Jason Culbertson <jason.culbertson@linktr.ee>
2025-01-14 19:22:21 -0800

    v6

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,5 +1,5 @@
 import type { APIRoute } from 'astro';
-import { supabaseAdmin } from '../../../lib/supabase';
+import { supabaseAdmin } from '../../lib/supabase';
 
 export const prerender = false;
 
