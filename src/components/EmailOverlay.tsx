af46edab897b7acab3a942096c695af074d94bef
Jason Culbertson <jason.culbertson@linktr.ee>
2025-01-15 23:02:53 -0800

    Update EmailOverlay.tsx

--- src/components/EmailOverlay.tsx
+++ src/components/EmailOverlay.tsx
@@ -71,7 +71,7 @@ export default function EmailOverlay({ onSuccess }: EmailOverlayProps) {
         Get unlimited access
       </h2>
       <p className="text-lg text-gray-600 mb-6">
-        Everything you need to lead design teams
+        Essential resources for design leaders
       </p>
 
       <div className="max-w-md mx-auto">
